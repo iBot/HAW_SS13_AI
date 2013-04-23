bff4975974b147043d64355b9ef36822b7c03452
iBot <iBot@no.mail>
2013-04-23 18:48:56 +0200

    PersistenzManagerTest

---
+++ code/HES/test/main/technik/persistenzManager/IPersistenzManagerTest.java
@@ -0,0 +1,49 @@
+package main.technik.persistenzManager;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tobi
+ * Date: 23.04.13
+ * Time: 18:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class IPersistenzManagerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testAccess() throws Exception {
+
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testReturnQuery() throws Exception {
+
+    }
+}
