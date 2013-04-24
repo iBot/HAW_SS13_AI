e4b034db617e337a0412573ddc02bc642d5e4b67
iBot <iBot@no.mail>
2013-04-24 09:42:12 +0200

    Testen...

--- code/HES/test/main/komponenten/verkauf/IVerkaufManagerTest.java
+++ code/HES/test/main/komponenten/verkauf/IVerkaufManagerTest.java
@@ -1,8 +1,17 @@
 package main.komponenten.verkauf;
 
+import junit.framework.Assert;
+import main.allgemeineTypen.transportTypen.AngebotTyp;
+import main.allgemeineTypen.transportTypen.AuftragTyp;
+import main.technik.persistenzManager.IPersistenzManager;
+import main.technik.persistenzManager.PersistenzManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: Tobi
@@ -10,9 +19,13 @@ import org.junit.Test;
  * Time: 14:29
  */
 public class IVerkaufManagerTest {
+
+    IPersistenzManager persistenzManager;
+    IVerkaufManager verkaufManager;
     @Before
     public void setUp() throws Exception {
-
+        persistenzManager = PersistenzManager.getInstance();
+        verkaufManager = new VerkaufFassade();
     }
 
     @After
@@ -22,11 +35,24 @@ public class IVerkaufManagerTest {
 
     @Test
     public void testErstelleAngebot() throws Exception {
-
+        Map<String, Integer> produkte = new HashMap<>();
+        produkte.put("Eins",1);
+        produkte.put("Zwei",2);
+        AngebotTyp erstelltesAngebot = verkaufManager.erstelleAngebot("KUNDE-blabla",new Date(), new Date(), produkte);
+        AngebotTyp gelesenesAngebot = verkaufManager.getAngebotZuID(erstelltesAngebot.getAngebotNr());
+        Assert.assertEquals("Angebot identisch: ",erstelltesAngebot,gelesenesAngebot);
     }
 
     @Test
     public void testErstelleAuftrag() throws Exception {
+        Map<String, Integer> produkte = new HashMap<>();
+        produkte.put("DREI",3);
+        produkte.put("VIER",4);
+        AngebotTyp angebot = new AngebotTyp("DER KUNDE",new Date(), new Date(),produkte);
 
+        AuftragTyp erstellterAuftrag = verkaufManager.erstelleAuftrag(angebot);
+
+        AuftragTyp gelesenerAuftrag = verkaufManager.getAuftragZuID(erstellterAuftrag.getAuftragsNr());
+        Assert.assertEquals("Auftrag identisch: ",erstellterAuftrag,gelesenerAuftrag);
     }
 }
