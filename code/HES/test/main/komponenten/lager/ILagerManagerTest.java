3546d939d5afc84db43ba65ec2ba4d7088155126
Milena Dreier <mille_3@hotmail.de>
2013-04-24 12:13:13 +0200

    ILagerManagerTest

--- code/HES/test/main/komponenten/lager/ILagerManagerTest.java
+++ code/HES/test/main/komponenten/lager/ILagerManagerTest.java
@@ -1,8 +1,16 @@
 package main.komponenten.lager;
 
+import junit.framework.Assert;
+import main.allgemeineTypen.transportTypen.AngebotTyp;
+import main.allgemeineTypen.transportTypen.AuftragTyp;
+import main.allgemeineTypen.transportTypen.ProduktTyp;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: Tobi
@@ -28,6 +36,21 @@ public class ILagerManagerTest {
 
     @Test
     public void testReserviereProdukteFuerAuftrag() throws Exception {
+        ProduktTyp tulpe = lagerManager.erstelleProdukt("Tulpe");
+        ProduktTyp rose = lagerManager.erstelleProdukt("Rose");
+        Map<String, Integer> produktListe = new HashMap<>();
+        produktListe.put(tulpe.getProduktNr(), 100);
+        produktListe.put(rose.getProduktNr(), 50);
+        Date heute = new Date();
+        AngebotTyp angebot = new AngebotTyp("KUN-1", heute, new Date(heute.getTime() + (24L*60*60*1000)), new HashMap<>(produktListe));
+        AuftragTyp auftrag = new AuftragTyp("ANG-1", false, heute);
 
+        lagerManager.reserviereProdukteFuerAuftrag(auftrag, angebot);
+
+        ProduktTyp changedTulpe = lagerManager.getProduktZuID(tulpe.getProduktNr());
+        ProduktTyp changedRose = lagerManager.getProduktZuID(rose.getProduktNr());
+
+        Assert.assertEquals(rose.getLagerbestand()-50, changedRose.getLagerbestand());
+        Assert.assertEquals(tulpe.getLagerbestand()-100, changedTulpe.getLagerbestand());
     }
 }
