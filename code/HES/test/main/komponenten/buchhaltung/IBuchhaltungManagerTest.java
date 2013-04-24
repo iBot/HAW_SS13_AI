e8f818d4538c8b8f46fd1047aa48a814cd25602e
iBot <iBot@no.mail>
2013-04-24 12:13:39 +0200

    Testen...

--- code/HES/test/main/komponenten/buchhaltung/IBuchhaltungManagerTest.java
+++ code/HES/test/main/komponenten/buchhaltung/IBuchhaltungManagerTest.java
@@ -1,10 +1,13 @@
 package main.komponenten.buchhaltung;
 
 import junit.framework.TestCase;
+import main.allgemeineTypen.transportTypen.AuftragTyp;
 import main.allgemeineTypen.transportTypen.RechnungTyp;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
 
 /**
  * User: Tobi
@@ -26,14 +29,14 @@ public class IBuchhaltungManagerTest extends TestCase {
 
     @Test
     public void testErstelleRechnung() throws Exception {
-        RechnungTyp rechnungTyp = buchhaltung.erstelleRechnung(50);
+        RechnungTyp rechnungTyp = buchhaltung.erstelleRechnung(50,new AuftragTyp("A1","A1",false,new Date()));
         RechnungTyp restoredRechnungTyp = buchhaltung.getRechnungZuID(rechnungTyp.getRechnungsNr());
         assertNotNull("Rechnung wurde erstellt: ",restoredRechnungTyp);
     }
 
     @Test
     public void testZahlungseingangBuchen() throws Exception {
-        RechnungTyp rechnungTyp = buchhaltung.erstelleRechnung(50);
+        RechnungTyp rechnungTyp = buchhaltung.erstelleRechnung(50,new AuftragTyp("A1","A1",false,new Date()));
         assertFalse("Rechnung Bezahlt: ", rechnungTyp.isIstBezahlt());
         buchhaltung.zahlungseingangBuchen(50, rechnungTyp.getRechnungsNr());
         RechnungTyp restoredRechnungTyp = buchhaltung.getRechnungZuID(rechnungTyp.getRechnungsNr());
