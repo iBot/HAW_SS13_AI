b62fb4d87e8c8e8fa67e83d3bb85cae2996896a8
iBot <iBot@no.mail>
2013-05-22 11:31:35 +0200

    Ich habe keine Ahnung, was ich tue...

--- code/HES/ClientInstanz/src/komponenten/AktiveRedundanz/monitor/MonitorLogik.java
+++ code/HES/ClientInstanz/src/komponenten/AktiveRedundanz/monitor/MonitorLogik.java
@@ -14,8 +14,8 @@ public class MonitorLogik {
     StatusEnum instanzStatus1 = StatusEnum.DEAD;
     StatusEnum instanzStatus2 = StatusEnum.DEAD;
     Timer timer = new Timer();
-    TimeOutTask timeOutTask1 = new TimeOutTask(this,1);
-    TimeOutTask timeOutTask2 = new TimeOutTask(this,2);
+    TimeOutTask timeOutTask1 = new TimeOutTask(this, 1);
+    TimeOutTask timeOutTask2 = new TimeOutTask(this, 2);
     long startTime = 0;
     long systemInstanz1Uptime = 0;
     long systemInstanz2Uptime = 0;
@@ -29,45 +29,40 @@ public class MonitorLogik {
         this.timeOut = timeOut;
         timer.schedule(timeOutTask1, timeOut);
         timer.schedule(timeOutTask2, timeOut);
-        timer.schedule(new TimeUpdateTask(this),0,1000);
+        timer.schedule(new TimeUpdateTask(this), 0, 1000);
         IRemoteIAmALive remoteIAmALive = new RemoteIAmLiveImpl(this);
-        Naming.rebind("remoteIamAlive",remoteIAmALive);
-        startTime=System.currentTimeMillis();
+        Naming.rebind("remoteIamAlive", remoteIAmALive);
+        startTime = System.currentTimeMillis();
         lastTimeSwitch1 = startTime;
         lastTimeSwitch2 = startTime;
     }
 
-
     public void schreibeFürInstanzStatusListenerEin(IStatusMonitorListener listener, int systemInstanzID) {
-        if(systemInstanzID == 1){
+        if (systemInstanzID == 1) {
             listenerRepository.setStatusMonitorListener1(listener);
-        }
-        else
+        } else
             listenerRepository.setStatusMonitorListener2(listener);
     }
 
     public void schreibeFürUptimeÄnderungEin(IMonitorListener listener, int systemInstanzID) {
-        if(systemInstanzID == 1){
+        if (systemInstanzID == 1) {
             listenerRepository.setMonitorListenerUptime1(listener);
-        }
-        else
+        } else
             listenerRepository.setMonitorListenerUptime2(listener);
     }
 
     public void schreibeFürDowntimeÄnderungEin(IMonitorListener listener, int systemInstanzID) {
-        if(systemInstanzID == 1){
+        if (systemInstanzID == 1) {
             listenerRepository.setMonitorListenerDowntime1(listener);
-        }
-        else
+        } else
             listenerRepository.setMonitorListenerDowntime2(listener);
     }
 
     public void setInstanceStatus(StatusEnum status, int systemInstanzID) {
         upTimeAktuallisieren();
-        if(systemInstanzID == 1){
+        if (systemInstanzID == 1) {
             instanzStatus1 = status;
-        }
-        else
+        } else
             instanzStatus2 = status;
 
         //Alle Status Listener Feuern
@@ -76,35 +71,39 @@ public class MonitorLogik {
     }
 
     public void iAmAlive(int systemInstanzID) {
-        if(systemInstanzID == 1){
+        if (systemInstanzID == 1) {
             timeOutTask1.cancel();
             timer.schedule(timeOutTask1, timeOut);
-        }
-        else
-            timeOutTask1.cancel();
+        } else {
+            timeOutTask2.cancel();
             timer.schedule(timeOutTask2, timeOut);
+        }
     }
 
-
-    private void upTimeAktuallisieren(){
-        if(instanzStatus1 == StatusEnum.ONLINE)
-            systemInstanz1Uptime+=System.currentTimeMillis()-lastTimeSwitch1;
-        if(instanzStatus2 == StatusEnum.ONLINE)
-            systemInstanz2Uptime+=System.currentTimeMillis()-lastTimeSwitch2;
+    private void upTimeAktuallisieren() {
+        if (instanzStatus1 == StatusEnum.ONLINE)
+            systemInstanz1Uptime += System.currentTimeMillis() - lastTimeSwitch1;
+        if (instanzStatus2 == StatusEnum.ONLINE)
+            systemInstanz2Uptime += System.currentTimeMillis() - lastTimeSwitch2;
     }
-    void timeListenerausführen(){
+
+    void timeListenerausführen() {
         upTimeAktuallisieren();
         calcDownTime();
-        if (listenerRepository.isMonitorListenerUptime1Initalized()) listenerRepository.getMonitorListenerUptime1().führeAktionAus(systemInstanz1Uptime);
-        if (listenerRepository.isMonitorListenerUptime2Initalized()) listenerRepository.getMonitorListenerUptime2().führeAktionAus(systemInstanz2Uptime);
-        if (listenerRepository.isMonitorListenerDowntime1Initalized()) listenerRepository.getMonitorListenerDowntime1().führeAktionAus(systemInstanz1Downtime);
-        if (listenerRepository.isMonitorListenerDowntime2Initalized()) listenerRepository.getMonitorListenerDowntime2().führeAktionAus(systemInstanz2Downtime);
+        if (listenerRepository.isMonitorListenerUptime1Initalized())
+            listenerRepository.getMonitorListenerUptime1().führeAktionAus(systemInstanz1Uptime);
+        if (listenerRepository.isMonitorListenerUptime2Initalized())
+            listenerRepository.getMonitorListenerUptime2().führeAktionAus(systemInstanz2Uptime);
+        if (listenerRepository.isMonitorListenerDowntime1Initalized())
+            listenerRepository.getMonitorListenerDowntime1().führeAktionAus(systemInstanz1Downtime);
+        if (listenerRepository.isMonitorListenerDowntime2Initalized())
+            listenerRepository.getMonitorListenerDowntime2().führeAktionAus(systemInstanz2Downtime);
     }
 
     // vorher sollte man systemInstanzUptime updaten
-    private void calcDownTime(){
-        systemInstanz1Downtime = startTime-systemInstanz1Uptime;
+    private void calcDownTime() {
+        systemInstanz1Downtime = startTime - systemInstanz1Uptime;
 
-        systemInstanz2Downtime = startTime-systemInstanz2Uptime;
+        systemInstanz2Downtime = startTime - systemInstanz2Uptime;
     }
 }
